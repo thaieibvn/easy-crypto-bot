3c0c052db8c82c6a43c4c8a433212f545f50b266
Stefan Hristov <hristov.sn@gmail.com>
2019-05-26 21:19:31 +0300

    Fix linux build script

--- build-lin.sh
+++ build-lin.sh
@@ -16,16 +16,16 @@ mv easy-crypto-bot-linux-x64 easy-crypto-bot-lin64
 
 mkdir easy-crypto-bot-lin32/assets
 mkdir easy-crypto-bot-lin32/assets/icons
-cp ../../assets/icons/icon.png easy-crypto-bot-lin32/assets/icons/icon.png
-cp ../../assets/icons/icon.ico easy-crypto-bot-lin32/assets/icons/icon.ico
+cp ../assets/icons/icon.png easy-crypto-bot-lin32/assets/icons/icon.png
+cp ../assets/icons/icon.ico easy-crypto-bot-lin32/assets/icons/icon.ico
 cp easy-crypto-bot-lin32/assets/icons/icon.png easy-crypto-bot-lin32/icon.png
 chmod +x easy-crypto-bot-lin32/easy-crypto-bot
 tar -czf easy-crypto-bot-lin32.tar.gz easy-crypto-bot-lin32/
 
 mkdir easy-crypto-bot-lin64/assets
 mkdir easy-crypto-bot-lin64/assets/icons
-cp ../../assets/icons/icon.png easy-crypto-bot-lin64/assets/icons/icon.png
-cp ../../assets/icons/icon.ico easy-crypto-bot-lin64/assets/icons/icon.ico
+cp ../assets/icons/icon.png easy-crypto-bot-lin64/assets/icons/icon.png
+cp ../assets/icons/icon.ico easy-crypto-bot-lin64/assets/icons/icon.ico
 cp easy-crypto-bot-lin64/assets/icons/icon.png easy-crypto-bot-lin64/icon.png
 chmod +x easy-crypto-bot-lin64/easy-crypto-bot
 tar -czf easy-crypto-bot-lin64.tar.gz easy-crypto-bot-lin64/
